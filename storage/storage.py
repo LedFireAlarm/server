73ff4b00c17abd4a9e09182940612e4adcc01fd6
ttwoti <ttwoti@gmail.com>
2023-02-25 14:57:24 +0300

    Add storage

--- storage/storage.py
+++ storage/storage.py
@@ -1,1 +1,21 @@
-# Storage for all events
+from peewee import SqliteDatabase, Model
+from peewee import CharField, DateTimeField, UUIDField, IntegerField
+import uuid
+
+db = SqliteDatabase("events")
+
+
+class BaseModel(Model):
+    uuid = UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    class Meta:
+        database = db
+
+
+class Events(BaseModel):
+    device_id = CharField()
+    date = DateTimeField()
+    alert_type = IntegerField()  # Lool in events.events.AlertEvents
+
+
+db.connect()
+db.create_tables([Events])
