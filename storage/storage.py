10d0b14cc72ac45f584f1480d2050b8b0d819fb5
ttwoti <ttwoti@gmail.com>
2023-02-25 22:09:46 +0300

    Add lenght in storage

--- storage/storage.py
+++ storage/storage.py
@@ -2,11 +2,11 @@ from peewee import SqliteDatabase, Model
 from peewee import CharField, DateTimeField, UUIDField, IntegerField
 import uuid
 
-db = SqliteDatabase("events")
+db = SqliteDatabase("events.db3")
 
 
 class BaseModel(Model):
-    uuid = UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    uuid = UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     class Meta:
         database = db
 
@@ -14,7 +14,8 @@ class BaseModel(Model):
 class Events(BaseModel):
     device_id = CharField()
     date = DateTimeField()
-    alert_type = IntegerField()  # Lool in events.events.AlertEvents
+    lenght = IntegerField(null = True)
+    alert_type = IntegerField(null=True)  # Lool in events.events.AlertEvents
 
 
 db.connect()
